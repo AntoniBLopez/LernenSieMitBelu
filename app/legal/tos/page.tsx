446ecd2e91af85141bbaa66a937d3ea18a49fb0f
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-05-09 01:21:51 +0200

    legal: bug fixed

--- app/legal/tos/page.tsx
+++ app/legal/tos/page.tsx
@@ -3,7 +3,7 @@
 export default function Privacypolicy() {
   return (
     <div className="w-full h-full bg-bluishBlackColor">
-      <section className="flex flex-col gap-10 mx-[30%] mt-16 bg-bluishBlackColor">
+      <section className="flex flex-col gap-10 mx-[30%] py-16">
         <h1>Terms of Service</h1>
         <div>
           content
