373fe05b284b18531684ad258bcdd76dc12bd96d
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-05-10 17:30:08 +0200

    Skeleton startup app

--- app/ui/Main.tsx
+++ app/ui/Main.tsx
@@ -3,9 +3,15 @@ export default function Main() {
   return (
     <main className="h-fit w-full">
       <div className="px-fixed w-full h-fit bg-bluishBlackColor">
-        <div className="h-screen py-10" id="pricing">
+        <div className="flex flex-wrap h-screen py-10" id="pricing">
+          <div className="w-[50%] h-full">
+            <h1>Main 1 - Headline</h1>
 
-          <h1>Main 1</h1>
+          </div>
+          <div className="w-[50%] h-full">
+            <h1>Main 1 - Demo</h1>
+
+          </div>
         </div>
       </div>
 
