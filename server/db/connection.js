67e5c52fb1167a6080de83ae983c63873a7c195b
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-05-22 17:49:44 +0200

    production server added

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,11 +3,11 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env.server') })
 
 const connection = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.NODE_ENV === 'development' ? process.env.DEV_DB_HOST : process.env.DB_HOST,
+  user: process.env.NODE_ENV === 'development' ? process.env.DEV_DB_USER : process.env.DB_USER,
+  password: process.env.NODE_ENV === 'development' ? process.env.DEV_DB_PASSWORD : process.env.DB_PASSWORD,
+  database: process.env.NODE_ENV === 'development' ? process.env.DEV_DB_NAME : process.env.DB_NAME,
+  port: process.env.NODE_ENV === 'development' ? process.env.DEV_DB_PORT : process.env.DB_PORT,
 })
 
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,21 +1,8 @@
-require('dotenv').config()
 const router = require('express').Router()
 const fs = require('fs')
 const path = require('path')
 const bcrypt = require('bcrypt')
 const { GetUsers, GetLevels, PostUser, PostLevel } = require('../db/queries')
-// const { create } = require('domain')
-// const { google } = require('googleapis')
-
-// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
-// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
-// const REFRESH_TOKEN = process.env.MY_REFRESH_TOKEN // DELETE
-
-// const oauth2Client = new google.auth.OAuth2(
-//   GOOGLE_CLIENT_ID,
-//   GOOGLE_CLIENT_SECRET,
-//   'http://localhost:3000' // Redirect URL
-// )
 
 router.get('/', async (req, res, next) => {
   res.status(206).json({ message: '/api endpoint server is running' })
