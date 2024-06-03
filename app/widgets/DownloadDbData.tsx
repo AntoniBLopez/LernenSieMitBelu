f2dd8f56fb1dc4e63167163342ece8633a8a426a
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-06-03 13:45:18 +0200

    bug fixed

--- app/widgets/DownloadDbData.tsx
+++ app/widgets/DownloadDbData.tsx
@@ -2,42 +2,43 @@
 
 import { useEffect, useState } from 'react';
 import { extractLevelsData } from '../admin/axios/queries';
+import { Levels } from '@/types';
 
 function DownloadDbData() {
-  const [jsonData, setJsonData] = useState(null)
+  const [jsonData, setJsonData] = useState<Levels | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await extractLevelsData()
-        setJsonData(data) // Almacenar los datos directamente, sin serializar
+        const data = await extractLevelsData();
+        setJsonData(data); // Almacenar los datos directamente, sin serializar
       } catch (error) {
-        console.error('Error fetching data:', error)
+        console.error('Error fetching data:', error);
       }
-    }
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const handleDownload = () => {
     if (jsonData) {
-      const blob = new Blob([jsonData], { type: 'application/json' })
-      const url = URL.createObjectURL(blob)
-      const a = document.createElement('a')
-      a.href = url
-      a.download = 'levels.json'
-      document.body.appendChild(a)
-      a.click()
-      document.body.removeChild(a)
-      URL.revokeObjectURL(url)
+      const jsonBlob = new Blob([JSON.stringify(jsonData)], { type: 'application/json' }); // Convertir jsonData a una cadena JSON
+      const url = URL.createObjectURL(jsonBlob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'levels.json';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
     }
-  }
+  };
 
   return (
     <button className='bg-selectedPrimaryColor text-grayColor font-medium py-2 px-4 rounded-lg' onClick={handleDownload}>
       Download DB Data
     </button>
-  )
+  );
 }
 
 export default DownloadDbData;
