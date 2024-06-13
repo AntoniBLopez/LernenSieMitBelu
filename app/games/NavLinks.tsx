0edc4a2b2ae56eb96ff737f99c65819d0a3c8bb8
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-06-13 11:14:33 +0200

    detail

--- app/games/NavLinks.tsx
+++ app/games/NavLinks.tsx
@@ -5,8 +5,6 @@ import {
   ClipboardDocumentListIcon,
   PuzzlePieceIcon,
   PencilSquareIcon,
-  PencilIcon
-
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
