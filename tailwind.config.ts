f90b54e845ce4b69d4e87e5820a12e0ee9ccad87
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-06-18 20:12:25 +0200

    trying to remove dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // darkMode: 'class', // Doesn't work
+  darkMode: 'class', // Doesn't work
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
